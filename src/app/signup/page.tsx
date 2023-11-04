a8fd30e1b1abab470187398d84f9cd20c5ae0ae0
Isacre <isaacalvesx7@gmail.com>
2023-11-03 21:18:11 -0300

    sign up route

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React from "react";
+import FeedLayout from "../layouts/layout";
+import styled from "styled-components";
+import Link from "next/link";
+const Wrapper = styled.div`
+  display: flex;
+  padding: 25px 0px;
+  align-items: baseline;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 500px;
+  height: 470px;
+  background-color: #404156;
+  border-radius: 5px;
+`;
+
+const Title = styled.div`
+  border-radius: 5px 5px 0px 0px;
+  background-color: #6a6c98;
+  padding: 20px;
+  text-align: center;
+  color: white;
+  font-weight: bolder;
+`;
+
+const FormBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 15px;
+
+  label {
+    margin-bottom: 10px;
+  }
+  input {
+    padding: 10px 10px;
+    outline: none;
+    border: none;
+    background-color: #eaf0ff;
+    border-radius: 2px;
+    margin-bottom: 10px;
+  }
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  padding: 15px;
+  background-color: #6dbcd5;
+  border: none;
+  border-radius: 2px;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+const Message = styled.b`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
+  color: #00c3ff;
+`;
+
+const SignUpRedirect = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 5px;
+  b {
+    cursor: pointer;
+    color: #00c3ff;
+  }
+`;
+export default function SignUp() {
+  return (
+    <FeedLayout>
+      <Wrapper>
+        <Form>
+          <Title>Sign Up</Title>
+          <Message>Join the best studying community</Message>
+          <FormBody>
+            <label htmlFor="">Email</label>
+            <input type="text" />
+            <label htmlFor="">Username</label>
+            <input type="text" />
+            <label htmlFor="">Password</label>
+            <input type="password" />
+            <SubmitButton>Submit</SubmitButton>
+            <SignUpRedirect>
+              <small>Haven't signed up yet?</small>
+              <Link href="/signup">
+                <b>Sign Up</b>
+              </Link>
+            </SignUpRedirect>
+          </FormBody>
+        </Form>
+      </Wrapper>
+    </FeedLayout>
+  );
+}

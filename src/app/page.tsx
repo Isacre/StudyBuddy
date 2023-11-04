f11dadd5a94acfc9ba0d1ff5d031af7aecb78c32
Isacre <isaacalvesx7@gmail.com>
2023-11-03 21:19:44 -0300

    changed root route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+import FeedLayout from "./layouts/layout";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  padding: 25px 0px;
+`;
+
 export default function Home() {
-  return <main>a</main>;
+  return (
+    <FeedLayout>
+      <Wrapper>a</Wrapper>
+    </FeedLayout>
+  );
 }

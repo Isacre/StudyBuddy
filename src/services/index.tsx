4d96fc2b5ab9d381646707f30f91dd291e282967
Isacre <isaacalvesx7@gmail.com>
2023-11-03 21:17:47 -0300

    declared types

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,4 +1,4 @@
-import { UserAccount } from "@/types";
+import { AuthToken, UserAccount } from "@/types";
 import axios from "axios";
 
 const url = process.env.REACT_APP_API_URL;
@@ -16,7 +16,10 @@ export async function registerAccount(email: string, username: string, password:
   return account.data;
 }
 
-export async function login(email: string, password: string): Promise<any> {}
+export async function login(email: string, password: string): Promise<AuthToken> {
+  const tokens = await axios.post(`${url}/auth/jwt/create/`, { email, password }, config);
+  return tokens.data;
+}
 
 export async function getUserData(): Promise<UserAccount> {
   const user = await axios.get(`${url}/auth/users/me/`, config);
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,8 +1,15 @@
+import { IconType } from "react-icons";
+
 export declare type UserAccount = {
   id: number;
   email: string;
   picture: string;
   username: string;
+} | null;
+
+export declare type AuthToken = {
+  access: string;
+  refresh: string;
 };
 
-export declare type AuthToken = {};
+export declare type ContextMenuOption = { text: string; icon: IconType; onClick: Function; color?: string };
